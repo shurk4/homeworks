7318fc43c1d3b295d3ea6eed2169ffc0d8fbdd27
shurk <shurk4@gmail.com>
2022-02-09 12:32:45 +0300

    less 14 upd 3

--- 14_3 snake list/main.cpp
+++ 14_3 snake list/main.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello" << std::endl;
+    int arr[5][5]{0};
+    int num = -1;
+    for(int i = 0; i < 5; i++){
+        if(i % 2 == 0) {
+            for (int j = 0; j < 5; j++) {
+                num++;
+                arr[i][j] = num;
+            }
+        } else {
+            for (int j = 4; j >= 0; j--) {
+                num++;
+                arr[i][j] = num;
+            }
+        }
+    }
+
+    for(int i = 0; i < 5; i++) {
+        for(int j = 0; j < 5; j++){
+            std::cout << arr[i][j] << "\t";
+        }
+        std::cout << "\n";
+    }
     return 0;
 }
