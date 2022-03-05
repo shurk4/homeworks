c8b885e3d56b74b2f0b2fa52b9b6acf72032dcf4
shurk <shurk4@gmail.com>
2022-03-05 13:51:47 +0300

    15_1 upd

--- 15_1/main.cpp
+++ 15_1/main.cpp
@@ -1,18 +1,37 @@
 #include <iostream>
 #include <vector>
 
+void masInput(std::vector<int> &mas){
+    int num, masSize;
+    do {
+        std::cout << "Enter the array size:\n";
+        std::cin >> masSize;
+        if(masSize < 1) std::cout << "Wrong input!\n";
+    } while (masSize < 1);
+
+    std::cout << "Enter the numbers:\n";
+    for(int i = 0; i < masSize; i++) {
+        std::cin >> num;
+        mas.push_back(num);
+    }
+}
+
 void masPrint(std::vector<int> mas){
     for(int i = 0; i < mas.size(); i++)
         std::cout << mas[i] << " ";
     std::cout << "\n";
 }
 
+
 int main() {
-    std::vector <int> mas {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+    std::vector <int> mas;
+//    std::vector <int> mas {-2, 1, -3, 4, -1, 2, 1, -5, 4};
 //    std::vector <int> mas {7, -5, 1, -3, 4, -1, 8, -2, 2, 1, -5, 4};
     int min = 0, max = 0, minResult = 0, maxResult = 0;
     int minIndex = mas.size() - 1, maxIndex = 0;
     int result = 0;
+
+    masInput(mas);
     masPrint(mas);
 
     for (int i = 0; i < mas.size(); i++){
