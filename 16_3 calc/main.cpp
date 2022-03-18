d502baf9dac2c126e6e31aa19fd448fa7dbf7d45
shurk <shurk4@gmail.com>
2022-03-18 18:32:38 +0300

    16_3 upd

--- 16_3 calc/main.cpp
+++ 16_3 calc/main.cpp
@@ -1,38 +1,34 @@
 #include <iostream>
 #include <cstdio>
+#include <sstream>
 
 bool itAction(char c) {
     return (c == '+' || c == '-' || c == '/' || c == '*');
 }
 
 int main() {
-    std::string input, first, second;
+    std::string input;
     char action = ' ';
-    double result;
+    double result, first, second;
     std::cout << "Welcome to the calculator!" << std::endl;
     std::cin >> input;
 
-    for(int i = 0; i < input.size(); i++) {
-        if(itAction(input[i]) && i > 0 && action == ' ') {
-            action = input[i];
-        } else if(action == ' ') {
-            first += input[i];
-        } else {
-            second += input[i];
-        }
-    }
+    std::stringstream inStream;
+    inStream << input;
+    inStream >> first >> action >> second;
+
+    std::cout << first << " " << action << " " << second << std::endl;
 
     if(action == '+'){
-        result = std::stod(first) + std::stod(second);
+        result = first + second;
     } else if(action == '-'){
-        result = std::stod(first) - std::stod(second);
+        result = first - second;
     } else if(action == '/'){
-        result = std::stod(first) / std::stod(second);
+        result = first / second;
     } else if(action == '*'){
-        result = std::stod(first) * std::stod(second);
+        result = first * second;
     }
 
     std::cout << first << " " << action << " " << second << " = " << result << std::endl;
-
     return 0;
 }
