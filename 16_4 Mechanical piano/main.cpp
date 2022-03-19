580287806d6619bb8cae76236b9611a467b31a5a
shurk <shurk4@gmail.com>
2022-03-19 12:09:52 +0300

    16_4 upd

--- 16_4 Mechanical piano/main.cpp
+++ 16_4 Mechanical piano/main.cpp
@@ -3,13 +3,13 @@
 #include <sstream>
 
 enum notes{
-    DO = 1,
-    RE,
-    MI,
-    FA,
-    SO,
-    LA,
-    SI
+    DO = 1 << 1,
+    RE = 1 << 2,
+    MI = 1 << 3,
+    FA = 1 << 4,
+    SO = 1 << 5,
+    LA = 1 << 6,
+    SI = 1 << 7
 };
 
 int main() {
@@ -22,13 +22,13 @@ int main() {
         std::cin >> input;
 
         for(int j = 0; j < input.size(); j++){
-            if((input[j] - '0') == DO) result << "DO ";
-            if((input[j] - '0') == RE) result << "RE ";
-            if((input[j] - '0') == MI) result << "MI ";
-            if((input[j] - '0') == FA) result << "FA ";
-            if((input[j] - '0') == SO) result << "SO ";
-            if((input[j] - '0') == LA) result << "LA ";
-            if((input[j] - '0') == SI) result << "SI ";
+            if((1 << (input[j] - '0')) == DO) result << "DO ";
+            if((1 << (input[j] - '0')) == RE) result << "RE ";
+            if((1 << (input[j] - '0')) == MI) result << "MI ";
+            if((1 << (input[j] - '0')) == FA) result << "FA ";
+            if((1 << (input[j] - '0')) == SO) result << "SO ";
+            if((1 << (input[j] - '0')) == LA) result << "LA ";
+            if((1 << (input[j] - '0')) == SI) result << "SI ";
         }
         result << "\n";
     }
