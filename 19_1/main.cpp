6eaa5fb2ae809c6a46d9a81bffc0dc26b3abf5ff
shurk <shurk4@gmail.com>
2022-03-25 20:00:58 +0300

    gitignore upd

---
+++ 19_1/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+
+int main() {
+    std::ifstream file;
+    std::vector<std::string> text;
+    file.open("text.txt");
+
+    while(!file.eof()){
+        std::string word;
+        file >> word;
+        text.push_back(word);
+    }
+
+    for(int i = 0; i < text.size(); i++){
+        std::cout << text[i] << " ";
+    }
+
+    return 0;
+}
