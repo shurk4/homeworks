a8b564be47ef80489ee597414bdfd1e1c76cfe8f
shurk <shurk4@gmail.com>
2022-04-04 19:20:00 +0300

    20_1

--- 20_1/main.cpp
+++ 20_1/main.cpp
@@ -21,7 +21,20 @@
 #include <fstream>
 #include <sstream>
 #include <string>
+#include <iomanip>
 
+// Проверка имён на сторонние символы
+bool nameValid(std::string& name){
+    for(int i = 0; i < name.length(); i++){
+        if((name[i] >= 'a' && name[i] <= 'z') || (name[i] >= 'A' && name[i] <= 'Z')){
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+// Проверка даты
 bool dateValid(std::string& date){
     if (date.length() < 10) {
         return false;
@@ -32,21 +45,75 @@ bool dateValid(std::string& date){
     }
 }
 
+// Проверка зарплаты
+bool salaryValid(std::string& salary){
+    bool point = false;
+    for(int i = 0; i < salary.length(); i++){
+        if((salary[i] >= '0' && salary[i] <= '9') || (salary[i] == '.' && i != '0')){
+            if(salary[i] == '.' && point == false){
+                point = i;
+            } else if(salary[i] == '.' && point == true){
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+    return true;
+}
+
+//Конвертация з.п. до сотых
+void salaryConvert(std::string& salary){
+    double salaryTemp;
+    std::stringstream stream;
+
+    salaryTemp = std::stod(salary);
+    stream << std::fixed << std::setprecision(2) << salaryTemp;
+    stream >> salary;
+}
+
 std::string input (){
     std::string dataIn,
-            dummy,
-            date;
-
+            firstName,
+            lastName,
+            date,
+            salary;
+    bool valid;
     do {
+        valid = true;
         std::stringstream temp;
+
         std::cout << "Enter the data in the format: First name last name date dd.mm.yyyy salary" << std::endl;
         std::getline(std::cin, dataIn);
         temp << dataIn;
-        temp >> dummy >> dummy >> date;
+
+        dataIn = "";
+        temp >> firstName >> lastName >> date >> salary;
+        temp.clear();
+
         if(!dateValid(date)) {
             std::cout << "Wrong date, try again!" << std::endl;
+            valid = false;
         }
-    } while (!dateValid(date));
+        if(!nameValid(firstName)) {
+            std::cout << "Wrong first name, try again!" << std::endl;
+            valid = false;
+        }
+        if(!nameValid(lastName)) {
+            std::cout << "Wrong last name, try again!" << std::endl;
+            valid = false;
+        }
+        if(!salaryValid(salary)) {
+            std::cout << "Wrong salary format, try again!" << std::endl;
+            valid = false;
+        }
+        if(valid) {
+            salaryConvert(salary);
+
+            temp << firstName << " " << lastName << " " << date << " " << salary;
+            std::getline(temp, dataIn);
+        }
+    } while (!valid);
 
     return dataIn;
 }
