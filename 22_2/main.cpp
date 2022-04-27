091eb271b5dc3f26020bf4d77ce10e488a3e8c16
shurk <shurk4@gmail.com>
2022-04-27 18:48:37 +0300

    22_2

---
+++ 22_2/main.cpp
@@ -0,0 +1,80 @@
+/*Напишите программу «Регистратура».
+
+На вход программе приходят строки с фамилиями или строка Next.
+ Пришедшие люди становятся в очередь, а по запросу Next необходимо вызвать в регистратуру человека с фамилией,
+ идущей первой в лексикографическом порядке (по алфавиту), и вывести его фамилию на экран. Фамилии пациентов могут повторяться.
+
+Каждый запрос (на добавление и вывод) должен работать за O(logn).
+
+
+
+Пример:
+
+← Sidorov
+
+← Ivanov
+
+← Ivanov
+
+← Petrov
+
+← Next
+
+→ Ivanov
+
+← Next
+
+→ Ivanov
+
+← Next
+
+→ Petrov
+*/
+
+#include <iostream>
+#include <map>
+
+void qeue (std::multimap<std::string, int>& registry){
+    for(auto i = registry.begin(); i != registry.end(); i++){
+        std::cout << i->first << std::endl;
+    }
+}
+
+void add (std::multimap<std::string, int>& registry, std::string& input){
+    registry.emplace(input, 0);
+}
+
+void next (std::multimap<std::string, int>& registry){
+    if(!registry.empty()) {
+        std::cout << registry.begin()->first << std::endl;
+        registry.erase(registry.begin());
+    }
+    else {
+        std::cout << "Qeue is empty!" << std::endl;
+    }
+}
+
+int main() {
+    std::multimap<std::string, int > registry;
+    std::string input;
+
+    do {
+        std::cout << "Ented data:" << std::endl;
+        std::cin >> input;
+
+        if(input == "next"){
+            next(registry);
+        }
+        else if (input == "qeue"){
+            qeue(registry);
+        }
+        else if (input == "q"){
+            std::cout << "Bye!" << std::endl;
+        }
+        else {
+            add(registry, input);
+        }
+
+    } while(input != "q");
+    return 0;
+}
