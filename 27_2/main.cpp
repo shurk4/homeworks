1be167f78c2494f68fcba14983622616e1a5d8b6
shurk <shurk4@gmail.com>
2022-06-08 22:31:09 +0300

    27_2 upd

--- 27_2/main.cpp
+++ 27_2/main.cpp
@@ -3,159 +3,36 @@
 
 enum colors
 {
-    RED = 1,
-    BLUE = 2,
-    GREEN = 4,
-    NONE = 6
+    RED,
+    BLUE,
+    GREEN,
+    NONE
 };
 
-class Circle
+class Figure
 {
 public:
 
-    double r;
-
-    void setCircleParam()
-    {
-        std::cout << "Enter the circle radius:";
-        std::cin >> r;
-    }
-
-    void getCircleArea()
-    {
-        std::cout << "The circle area: " << atan(1) * r * r << std::endl;
-    }
-
-    void circleRectangularField()
-    {
-        std::cout << "Rectangular field size" << std::endl;
-        std::cout << "X: " << r * 2 << std::endl;
-        std::cout << "Y: " << r * 2 << std::endl;
-    }
-};
-
-class Square
-{
-public:
-
-    double edgeSize;
-
-    void setSquareParam()
-    {
-        std::cout << "Enter the length of the the square edge:";
-        std::cin >> edgeSize;
-    }
-
-    void getSquareArea()
-    {
-        std::cout << "The square area: " << edgeSize * edgeSize << std::endl;
-    }
-
-    void squareRectangularField()
-    {
-        std::cout << "Rectangular field size" << std::endl;
-        std::cout << "X: " << edgeSize << std::endl;
-        std::cout << "Y: " << edgeSize << std::endl;
-    }
-};
-
-class Triangle
-{
-public:
-
-    double edgeSize;
-    double fieldX;
-    double fieldY;
-
-    void setTriangleParam()
-    {
-        std::cout << "Enter the length of the triangle edge :";
-        std::cin >> edgeSize;
-        fieldX = edgeSize / 2;
-        fieldY = (edgeSize * std::sqrt(3)) / 2;
-    }
-
-    void getTriangleArea()
-    {
-        std::cout << "The triangle area: " << edgeSize * edgeSize * std::sqrt(3) / 4 << std::endl;
-    }
-
-    void triangleRectangularField()
-    {
-        std::cout << "Rectangular field size" << std::endl;
-        std::cout << "X: " << fieldX << std::endl;
-        std::cout << "Y: " << fieldY << std::endl;
-    }
-};
-
-class Rectangle
-{
-public:
-
-    double sizeX;
-    double sizeY;
-
-    void setRectangleParam()
-    {
-        std::cout << "Enter the size of the rectangle:" << std::endl;
-        std::cout << "X: ";
-        std::cin >> sizeX;
-        std::cout << "Y: ";
-        std::cin >> sizeY;
-    }
-
-    double getRectangleArea()
-    {
-        std::cout << "The rectangle area: " << sizeX * sizeY << std::endl;
-    }
-
-    void rectangleRectangularField()
-    {
-        std::cout << "Rectangular field size" << std::endl;
-        std::cout << "X: " << sizeX << std::endl;
-        std::cout << "Y: " << sizeY << std::endl;
-    }
-};
-
-class Figure:public Circle, public Square, public Triangle, public Rectangle
-{
     std::string type;
-
-public:
-
-    Figure(std::string &input)
-    {
-        type = input;
-        setGeneralParam();
-
-        if(type == "circle")
-        {
-            setCircleParam();
-        }
-        if(type == "square")
-        {
-            setSquareParam();
-        }
-        if(type == "triangle")
-        {
-            setTriangleParam();
-        }
-        if(type == "rectangle")
-        {
-            setRectangleParam();
-        }
-    }
-
-    std::string input;
     double centerX;
     double centerY;
+    double sizeX;
+    double sizeY;
+    double area;
     colors color;
 
-    void setColor()
+    void setBasic(std::string figureType)
     {
+        type = figureType;
+        std::string input;
+        std::cout << "Enter the coordinates of the " << type << " center" << std::endl;
+        std::cout << "X: ";
+        std::cin >> centerX;
+        std::cout << "Y: ";
+        std::cin >> centerY;
+
         std::cout << "Enter the color of figure(red, blue, green, none):" << std::endl;
         std::cin >> input;
-
         if(input == "red")
         {
             color = RED;
@@ -174,23 +51,10 @@ public:
         }
     }
 
-    void setCenter()
+    void print(double inFieldX, double inFieldY)
     {
-        std::cout << "Enter the coordinates of the center" << std::endl;
-        std::cout << "X: ";
-        std::cin >> centerX;
-        std::cout << "Y: ";
-        std::cin >> centerY;
-    }
-
-    void setGeneralParam()
-    {
-        setColor();
-        setCenter();
-    }
-
-    void getColor()
-    {
+        std::cout << "-------------------------" << std::endl;
+        std::cout << "The " << type << " area: " << area << std::endl;
         std::cout << "Color:\t\t";
         if(color == RED)
         {
@@ -209,46 +73,92 @@ public:
             std::cout << "none";
         }
         std::cout << std::endl;
-    }
-
-    void getStartPos(double x, double y)
-    {
+        std::cout << "Rectangular field size:" << std::endl;
+        std::cout << "X:\t\t" << inFieldX << std::endl;
+        std::cout << "Y:\t\t" << inFieldY << std::endl;
         std::cout << "Starting coordinates of rectangular field" << std::endl;
-        std::cout << "X: " <<  centerX - (x / 2) << " Y: " << centerY - (y / 2) << std::endl;
+        std::cout << "X:  " <<  centerX - (inFieldX / 2) << " Y:  " << centerY - (inFieldY / 2) << std::endl;
+        std::cout << "-------------------------" << std::endl;
+    }
+};
+
+class Circle:Figure
+{
+public:
+
+    Circle()
+    {
+        setBasic("circle");
+        std::cout << "Enter the circle radius:";
+        std::cin >> sizeX;
+        area = atan(1) * sizeX * sizeX;
     }
 
-    void result()
+    void getResult()
     {
-        std::cout << "----------------------" << std::endl;
-        std::cout << "Figure type:\t" << type << std::endl;
+        print(sizeX * 2, sizeX * 2);
+    }
+};
 
-        getColor();
+class Square:Figure
+{
+public:
 
-        if(type == "circle")
-        {
-            getCircleArea();
-            circleRectangularField();
-            getStartPos(r * 2, r * 2);
-        }
-        if(type == "square")
-        {
-            getSquareArea();
-            squareRectangularField();
-            getStartPos(Square::edgeSize, Square::edgeSize);
-        }
-        if(type == "triangle")
-        {
-            getTriangleArea();
-            triangleRectangularField();
-            getStartPos(fieldX, fieldY);
-        }
-        if(type == "rectangle")
-        {
-            getRectangleArea();
-            rectangleRectangularField();
-            getStartPos(Rectangle::sizeX, Rectangle::sizeY);
-        }
-        std::cout << "----------------------" << std::endl;
+    Square()
+    {
+        setBasic("square");
+        std::cout << "Enter the length of the square edge:";
+        std::cin >> sizeX;
+        area = sizeX * sizeX;
+    }
+
+    void getResult()
+    {
+        print(sizeX, sizeX);
+    }
+};
+
+class Triangle:Figure
+{
+    double fieldX;
+    double fieldY;
+
+public:
+
+    Triangle()
+    {
+        setBasic("triangle");
+        std::cout << "Enter the length of the triangle edge :";
+        std::cin >> sizeX;
+        fieldX = sizeX / 2;
+        fieldY = (sizeX * std::sqrt(3)) / 2;
+        area = sizeX * sizeX * std::sqrt(3) / 4;
+    }
+
+    void getResult()
+    {
+        print(fieldX, fieldY);
+    }
+
+};
+
+class Rectangle:Figure
+{
+public:
+    Rectangle()
+    {
+        setBasic("rectangle");
+        std::cout << "Enter the size of the rectangle:" << std::endl;
+        std::cout << "X: ";
+        std::cin >> sizeX;
+        std::cout << "Y: ";
+        std::cin >> sizeY;
+        area = sizeX * sizeY;
+    }
+
+    void getResult()
+    {
+        print(sizeX, sizeY);
     }
 };
 
@@ -257,12 +167,28 @@ int main() {
 
     std::cout << "Enter the figure type:" << std::endl;
     std::cin >> input;
+    if(input == "circle")
+    {
+        Circle *circle = new Circle;
+        circle->getResult();
+    }
+    if(input == "square")
+    {
+        Square *square = new Square;
+        square->getResult();
+    }
+    if(input == "triangle")
+    {
+        Triangle *triangle = new Triangle;
+        triangle->getResult();
+    }
+    if(input == "rectangle")
+    {
+        Rectangle *rectangle = new Rectangle;
+        rectangle->getResult();
+    }
 
-    Figure *figure = new Figure(input);
 
-    figure->result();
-
-    delete figure;
 
     return 0;
 }
