652a63f99c14e500b6296933fb4f4e592afe22cb
shurk <shurk4@gmail.com>
2022-06-17 11:25:13 +0300

    28_2

---
+++ 28_2/main.cpp
@@ -0,0 +1,91 @@
+/*С помощью многопоточности реализуйте простую модель железнодорожного вокзала.
+
+С трёх точек отправления в разное время отбывают три разных поезда с условными обозначениями A, B, C.
+ Все они идут на один и тот же вокзал и могут дойти до него в разное время.
+
+На этом целевом вокзале единовременно может находиться только один поезд.
+ Остальные поезда должны ждать, пока уже занятое место освободится.
+
+В начале программы пользователь последовательно вводит для каждого поезда время в пути до вокзала (в секундах).
+ После этого поезда начинают своё движение за заданное количество времени.
+ Как только поезд прибыл на вокзал, он ожидает от пользователя команды depart,
+ которая сигнализирует о его отбытии с вокзала.
+
+По всем событиям (прибытие на вокзал, ожидание свободного места, отбытие с вокзала)
+ вместе с условным обозначением поезда выводятся сообщения в консоль.
+
+Программа завершается, когда на вокзале побывают все три поезда.*/
+
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include <vector>
+
+std::mutex station;
+
+class Train
+{
+    int number;
+    int travelTime;
+
+public:
+
+    Train(int &num):number(num + 1)
+    {
+        std::cout << "Enter the travel time of the train " << number << ": " << std::endl;
+        std::cin >> travelTime;
+    }
+
+    int getNumber()
+    {
+        return number;
+    }
+
+    int getTravelTime()
+    {
+        return travelTime;
+    }
+};
+
+void move(Train *train)
+{
+    std::string command;
+    std::this_thread::sleep_for(std::chrono::seconds(train->getTravelTime()));
+    station.lock();
+    std::cout << "The train " << train->getNumber() << " has arrived at the station!" <<std::endl;
+    do
+    {
+        std::cout << "Enter the depart command: ";
+        std::cin >> command;
+        if(command != "depart")
+        {
+            std::cout << "Wrong input! Try again." << std::endl;
+        }
+    } while (command != "depart");
+    station.unlock();
+}
+
+int main() {
+    Train **trains = new Train*[3];
+    std::vector<std::thread> threads;
+
+    for(int i = 0; i < 3; i++)
+    {
+        trains[i] = new Train(i);
+    }
+
+    for(int i = 0; i < 3; i++)
+    {
+        threads.push_back(std::thread (move, std::cref(trains[i])));
+    }
+
+    for(int i = 0; i < 3; i++)
+    {
+        if(threads[i].joinable())
+        {
+            threads[i].join();
+        }
+    }
+
+    return 0;
+}
